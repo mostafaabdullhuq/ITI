2d2017448b396a25f47a8634160378ae7a62a5dc
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2022-11-17 16:10:11 +0200

    Assignment 1&2 JS_Day_1

--- Client_Side/Day_1_JavaScript/js/script.js
+++ Client_Side/Day_1_JavaScript/js/script.js
@@ -9,21 +9,17 @@ Confirm the full name.
 Ask the user to enter his birth year.
 Welcome the user using his full name and age.
 (Ex: Welcome Ahmed Hossam you are 35years old)
-*/
-
-    // declare required variables
-    let fName, lName, birthDate;
+    */
 
     // get user first name via prompt
-    fName = prompt("Enter First Name");
-
-    // get user last name via prompt
-    lName = prompt("Enter Last Name");
+    let fName = prompt("Enter First Name"),
+        // get user last name via prompt
+        lName = prompt("Enter Last Name");
 
     // if both first name and last name are given
     if (fName && lName) {
         // get user birth date via prompt
-        birthDate = prompt("Enter Birth Date");
+        let birthDate = prompt("Enter Birth Date");
 
         // if birthdate is given
         if (birthDate) {
@@ -41,34 +37,21 @@ Show a message to tell the user that it’s the first release of a calculator th
 Ask the user to enter the first number
 Ask the user to enter second number
 Show the summation result (ex: 30 + 6 = 36)
-*/
+    */
 
     // function to add two numbers and return the result of the sum
     let sum = (n1, n2) => {
         return n1 + n2;
     };
 
-    // declare required variables
-    let num1, num2;
-
     // show a message to user
     alert("Welcome, this is the first release of a calculator that only has a summation feature.");
 
     // get first number from user
-    num1 = Number(prompt("Enter first number"));
+    let num1 = Number(prompt("Enter first number")),
+        // get second number from user
+        num2 = Number(prompt("Enter second number"));
 
-    // get second number from user
-    num2 = Number(prompt("Enter second number"));
-
-    // if both numbers are given and is a number
-    if (num1 && num2) {
-        // display an alert with a message of the sum
-        alert(`${num1} + ${num2} = ${sum(num1, num2)}`);
-    }
-
-    // if numbers are not given or a string is given
-    else {
-        // display an alert with a message
-        alert("Please enter valid numbers");
-    }
+    // antonymous function: if both inputs are numbers print the sum of them, else show a message to user
+    num1 && num2 ? alert(`${num1} + ${num2} = ${sum(num1, num2)}`) : alert("Please enter valid numbers");
 }
