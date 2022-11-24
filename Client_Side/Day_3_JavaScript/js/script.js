b813e8e738987f0d976da139c1324759747da3d3
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2022-11-24 09:29:31 +0200

    Structuring

---
+++ Client_Side/Day_3_JavaScript/js/script.js
@@ -0,0 +1,37 @@
+// wait until the page loads, then invoke this function
+$(function () {
+    /* 
+- Assignment [1] 
+
+
+*/
+    // when assignment 1 button clicked, invoke this arrow func.
+    $("button#assignment_1").click((e) => {
+        // prevent the default behavior of the button
+        e.preventDefault();
+    });
+
+    /* 
+- Assignment [2] 
+
+
+
+*/
+
+    $("button#assignment_2").click((e) => {
+        // prevent the default behavior of the button
+        e.preventDefault();
+    });
+
+    /* 
+- Assignment [3] 
+
+
+
+*/
+
+    $("button#assignment_3").click((e) => {
+        // prevent the default behavior of the button
+        e.preventDefault();
+    });
+});
