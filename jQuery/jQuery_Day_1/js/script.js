8332c949a355d3b9fb134dc67fb20c746b66f3bd
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2022-12-15 15:19:03 +0200

    day_1_jQuery

---
+++ jQuery/jQuery_Day_1/js/script.js
@@ -0,0 +1,21 @@
+$(() => {
+    let imagesList = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg"],
+        currentIndex = 0,
+        mySlider = setInterval(() => {
+            $(".task1 img")
+                .fadeOut(500, function (e) {
+                    $(this).attr("src", `imgs/${imagesList[currentIndex === imagesList.length ? (currentIndex = 0) : currentIndex]}`);
+                })
+                .fadeIn(1000);
+            currentIndex++;
+        }, 3000);
+
+    $(".task1 .stop").on("click", (e) => {
+        console.log("cleared");
+        clearInterval(mySlider);
+    });
+
+    $(".task2 img").on("click", function (e) {
+        $(this).next().slideToggle();
+    });
+});
