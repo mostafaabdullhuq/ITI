d4057a63047f343d09c68e0c8848ece07016f662
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2022-12-19 03:52:31 +0200

    day_1_jquery, day_2_jquery

---
+++ jQuery/jQuery_Day_2/js/script.js
@@ -0,0 +1,48 @@
+$(() => {
+    function callback() {
+        // remove generated images for styling that causing element to change it's position
+        $(".ui-effects-placeholder").remove();
+    }
+
+    let shakeInterval = null;
+
+    // for rabbit image
+    $("#draggable").draggable({
+        revert: true,
+        // when dragging start, change text
+        start: function (e, targetElement) {
+            $(targetElement.helper).find("p").text("Leave meeeee!!!!!!");
+            var rabbitImage = $(targetElement.helper).find("img");
+            // start the shake immediately
+            rabbitImage.effect("shake", 500, callback);
+
+            // shake every 500ms
+            shakeInterval = setInterval(() => {
+                rabbitImage.effect("shake", 500, callback);
+            }, 500);
+        },
+
+        // when dragging finished
+        stop: function (e, targetElement) {
+            // stop the shake
+            clearInterval(shakeInterval);
+            // change the text
+            $(targetElement.helper).find("p").text("Drag me!");
+        },
+    });
+
+    // for black hole image
+    $("#droppable").droppable({
+        drop: function (e, draggedItem) {
+            // when rabbit image is on the black hole image, change the text
+            $(draggedItem.draggable).find("p").text("Nooooooooooo");
+
+            // when rabbit image is on the black hole image, fade out
+            $(draggedItem.draggable).hide("fade", {}, 2000);
+            setTimeout(() => {
+                // after 1 second, fade in
+                $(draggedItem.draggable).fadeIn();
+            }, 1000);
+        },
+    });
+});
---
+++ jQuery/jQuery_Day_2/js/todo.js
@@ -0,0 +1,60 @@
+$(function () {
+    let todosContainer = $(".todos-container"),
+        todoInput = $(".todo-text-input");
+
+    // when add todo button is clicked
+    $(".add-todo").on("click", function () {
+        // get the todo text
+        let todoText = todoInput.val();
+
+        // if todo text is not empty
+        if (todoText) {
+            todoInput.removeClass("invalid");
+
+            // reset the input text
+            todoInput.val("");
+            // create new todo and append it to the page
+            let todo = `
+                <div class="todo d-flex col py-2 mb-3">
+                    <div class="todo-text px-4 py-3 col rounded-2 me-3 flex-grow-1 fs-5 ">${todoText}</div>
+                    <button class="me-3 rounded-2 complete fs-6">Complete</button>
+                    <button class="rounded-2 delete fs-6">Delete</button>
+                </div>`;
+
+            todosContainer.append(todo);
+            // animate the created todo
+            $(".todo").last().hide().effect("slide", "slow");
+
+            // when complete button is clicked
+            $(".complete").on("click", function () {
+                // get the todo text
+                let currentTodoText = $(this).siblings(".todo-text");
+                // if todo text is not striked
+                if (!currentTodoText.hasClass("striked")) {
+                    // strike the todo text
+                    currentTodoText.addClass("striked");
+                    // change the complete button text
+                    $(this).text("Uncomplete");
+                    $(this).css("background-color", "#26ae7c");
+                } else {
+                    // un-strike the todo text
+                    currentTodoText.removeClass("striked");
+                    // change the complete button text
+                    $(this).text("Complete");
+                    $(this).css("background-color", "#3164d1");
+                }
+            });
+            // when delete button is clicked
+            $(".delete").on("click", function () {
+                // remove the todo
+                $(this)
+                    .parent()
+                    .hide("slide", "slow", function () {
+                        $(this).remove();
+                    });
+            });
+        } else {
+            todoInput.addClass("invalid");
+        }
+    });
+});
