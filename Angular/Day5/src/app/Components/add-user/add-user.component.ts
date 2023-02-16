171e52afd75067e568142641981b2a477e27d4e3
Mostafa Abdullhuq <mostafaabdullhuq@gmail.com>
2023-02-16 20:48:31 +0200

    added forms validation in lab5

--- Angular/Day5/src/app/Components/add-user/add-user.component.ts
+++ Angular/Day5/src/app/Components/add-user/add-user.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/Services/user.service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-user',
@@ -14,6 +15,65 @@ export class AddUserComponent {
     public myRoute: ActivatedRoute,
     public httpClient: UserService
   ) {}
+
+  public registerForm = new FormGroup({
+    name: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+    ]),
+
+    age: new FormControl(null, [
+      Validators.required,
+      Validators.min(16),
+      Validators.max(100),
+    ]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    phone: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(20),
+    ]),
+    address: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+    ]),
+  });
+
+  get isInputsEmpty() {
+    return (
+      this.registerForm.controls['name'].value === null &&
+      this.registerForm.controls['age'].value === null &&
+      this.registerForm.controls['email'].value === null &&
+      this.registerForm.controls['phone'].value === null &&
+      this.registerForm.controls['address'].value === null
+    );
+  }
+  get isNameValid() {
+    return this.registerForm.controls['name'].valid;
+  }
+
+  get isAgeValid() {
+    return this.registerForm.controls['age'].valid;
+  }
+
+  get isEmailValid() {
+    return this.registerForm.controls['email'].valid;
+  }
+
+  get isPhoneValid() {
+    return this.registerForm.controls['phone'].valid;
+  }
+
+  get isAddressValid() {
+    return this.registerForm.controls['address'].valid;
+  }
+
+  get isFormValid() {
+    return this.registerForm.valid;
+  }
+
   addUser(name: any, age: any, email: any, phone: any, address: any) {
     let user = {
       name: name,
--- Angular/Day5/src/app/Components/update-user/update-user.component.ts
+++ Angular/Day5/src/app/Components/update-user/update-user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/Services/user.service';
 
@@ -10,19 +11,20 @@ import { UserService } from 'src/app/Services/user.service';
 export class UpdateUserComponent implements OnInit {
   public user: any;
   public userID: any;
-
   constructor(
     private router: Router,
     public myRoute: ActivatedRoute,
     public httpClient: UserService
-  ) {}
+  ) {
+    this.user = {};
+  }
+
+  // [** function that communicates the service and get the user details on component initialization **] //
 
   ngOnInit(): void {
     this.userID = this.myRoute.snapshot.params['id'];
-
     this.httpClient.getUserById(this.userID).subscribe({
       next: (data) => {
-        console.log(data);
         this.user = data;
       },
       error: (err) => {
@@ -30,6 +32,8 @@ export class UpdateUserComponent implements OnInit {
       },
     });
   }
+
+  // [** function that communicates service to update user details in backend **] //
 
   updateUser(name: any, age: any, email: any, phone: any, address: any) {
     this.user = {
@@ -53,5 +57,71 @@ export class UpdateUserComponent implements OnInit {
         console.log(err);
       },
     });
+  }
+
+  // [** form validation, form controls initialized because data of user is loaded after the form check **] //
+
+  public updateForm = new FormGroup({
+    name: new FormControl('Lorem Ipsum', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+    ]),
+
+    age: new FormControl(22, [
+      Validators.required,
+      Validators.min(16),
+      Validators.max(100),
+    ]),
+    email: new FormControl('example@admin.com', [
+      Validators.required,
+      Validators.email,
+    ]),
+    phone: new FormControl('12345678910', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(20),
+    ]),
+    address: new FormControl('1234 Lorem st.', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(50),
+    ]),
+  });
+
+  // [** getters for conditionals form validations in html **] //
+
+  get isInputsEmpty() {
+    return (
+      this.updateForm.controls['name'].value === null &&
+      this.updateForm.controls['age'].value === null &&
+      this.updateForm.controls['email'].value === null &&
+      this.updateForm.controls['phone'].value === null &&
+      this.updateForm.controls['address'].value === null
+    );
+  }
+
+  get isNameValid() {
+    return this.updateForm.controls['name'].valid;
+  }
+
+  get isAgeValid() {
+    return this.updateForm.controls['age'].valid;
+  }
+
+  get isEmailValid() {
+    return this.updateForm.controls['email'].valid;
+  }
+
+  get isPhoneValid() {
+    return this.updateForm.controls['phone'].valid;
+  }
+
+  get isAddressValid() {
+    return this.updateForm.controls['address'].valid;
+  }
+
+  get isFormValid() {
+    return this.updateForm.valid;
   }
 }
